ca94c58970085c6f3405193a67b2937ad901c243
Katy St Sauveur <katyrogowski@yahoo.com>
2020-10-24 17:24:42 -0700

    Initial commmit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      rentals = []
+    }
+  }
+  render() {
+    <div className='App'>
+      <h1> rentals </h1>
+    </div>
+  }
+}
+
+export default App;
